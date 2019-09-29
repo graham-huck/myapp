a1a76c5612b44f1266a2c174eea18195b60ece92
Graham Huck <graham.huck@gmail.com>
2019-09-29 07:06:41 -0500

    Added edit and destroy actions. Cleaned up other actions

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,26 +1,45 @@
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.all
   end
-  def new
-    @hello = "blah"
-  end
-  def create
-    @animal = Animal.create!(animal_params)
-    redirect_to @animal
-  end
-  def edit
-    @id = params[:id]
-  end
+
   def show
     @animal = Animal.find(params[:id])
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to @animal
+    else
+      render 'new'
+    end
+  end
+
   def update
-    @id = params[:id]
-    @animal = Animals.new(params[:animal])
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to @animal
+    else
+      render 'edit'
+    end
   end
+
   def destroy
-    @id = params[:id]
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+
+    redirect_to animals_path
   end
+
   private
   def animal_params
     params.require(:animal).permit(:name, :credit, :image)
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,6 @@
 class Animal < ApplicationRecord
     has_one_attached :image
+    validates :name, presence: true
+    validates :image, presence: true
+    validates :credit, presence: true
 end
